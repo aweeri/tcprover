2b3bb3b952782ffdd344ed6502918ce8c96e9e6f
aweeri <aria@aweeri.com>
2025-02-24 02:23:25 +0100

    change

--- sender.py
+++ sender.py
@@ -9,14 +9,14 @@ def send_command(command, host='192.168.1.27', port=5000):
         s.sendall(command.encode('utf-8'))
         # Receive the response from the server
         response = s.recv(1024)
-        print(f"Rover: {response.decode('utf-8')}")
+        print(f"> {response.decode('utf-8')}")
 
 if __name__ == "__main__":
     # Example command input
     
     while True:
-        try:
-            command = input("Enter a command to send: ")
-            send_command(command)
-        except:
-            print("Failed to send command, try again.")
+            command = input("\033[31m$ \033[0m") 
+            try:
+                send_command(command)
+            except:
+                print("Failed to send command, try again.")
